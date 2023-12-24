a9c4f8ded8c121b1da3f0e6baf6b8252c600b71d
Bhopal Singh <bhopalsinghsis@gmail.com>
2023-12-24 10:08:20 -0500

    Spring Framework - Spring Resource Server Security Configuration

---
+++ spring-6-rest-mvc-resource-server-security-config/src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring6restmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Configuration
+public class SpringSecConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and().oauth2ResourceServer().jwt();
+
+        return http.build();
+    }
+
+}
